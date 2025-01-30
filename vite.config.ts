0845212fdbb0a039ba04146a5c65bd94cb53ec3a
tobenot <2818373670@qq.com>
2025-01-31 00:08:20 +0800

    bugfix: new api url

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,6 @@ export default defineConfig({
   define: {
     "process.env": {
       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      VITE_API_URL: JSON.stringify(
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:3000"
-          : "https://magic.tobenot.top",
-      ),
     },
   },
-  envDir: "./env",
 });
